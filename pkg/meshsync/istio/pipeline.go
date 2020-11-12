5bafc92183c2e561ced7da10a7a051b4cc8a5d83
Dev Kalra <kalradev@outlook.com>
2020-11-12 14:14:01 +0530

    implemented Virtual_service and pipeline to do discovery using common clients
    Signed-off-by: Dev Kalra <kalradev@outlook.com>

--- pkg/meshsync/istio/pipeline.go
+++ pkg/meshsync/istio/pipeline.go
@@ -1,7 +1,10 @@
 package istio
 
 import (
+	common "github.com/layer5io/meshery-operator/pkg/common"
+	client "github.com/layer5io/meshery-operator/pkg/meshsync/istio/client"
 	"github.com/myntra/pipeline"
+	"k8s.io/client-go/rest"
 )
 
 var (
@@ -13,7 +16,13 @@ var (
 // it will just get the steps and we will arrange them here
 
 // New will return a Pipeline
-func New() *pipeline.Pipeline {
+func New(config *rest.Config, kubeClient *common.KubeClient) (*pipeline.Pipeline, error) {
+	// create istio client
+	istioClient, err := client.NewIstioClientForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
 	// new pipeline
 	istioPipeline := pipeline.New("istio-discovery", 1000)
 
@@ -21,12 +30,15 @@ func New() *pipeline.Pipeline {
 	// stage-1
 	stage1 := pipeline.NewStage("stage-1", sequential, false)
 	// creating steps for this stage
-	step1 := &VirtualService{}
+	step1 := &VirtualService{
+		client:     istioClient,
+		kubeclient: kubeClient,
+	}
 	// adding steps to  the  stage
 	stage1.AddStep(step1)
 
 	// adding stages to pipeline
 	istioPipeline.AddStage(stage1)
 
-	return istioPipeline
+	return istioPipeline, nil
 }
--- pkg/meshsync/istio/virtual_service.go
+++ pkg/meshsync/istio/virtual_service.go
@@ -3,12 +3,17 @@ package istio
 import (
 	"log"
 
+	common "github.com/layer5io/meshery-operator/pkg/common"
+	client "github.com/layer5io/meshery-operator/pkg/meshsync/istio/client"
 	"github.com/myntra/pipeline"
 )
 
 // VirtualService will implement step interface for VirtualService
 type VirtualService struct {
 	pipeline.StepContext
+	// clients
+	client     *client.IstioClient
+	kubeclient *common.KubeClient
 }
 
 // Exec - step interface
@@ -16,10 +21,32 @@ func (vs *VirtualService) Exec(request *pipeline.Request) *pipeline.Result {
 	// it will contain a pipeline to run
 	log.Println("Virtual Service Discovery Started")
 
+	// get all namespaces
+	namespaces, err := vs.kubeclient.ListNamespace()
+	if err != nil {
+		return &pipeline.Result{
+			Error: err,
+		}
+	}
+
+	// virtual service for all namespace
+	for _, namespace := range namespaces {
+		virtualServices, err := vs.client.ListVirtualService(namespace.Name)
+		if err != nil {
+			return &pipeline.Result{
+				Error: err,
+			}
+		}
+
+		// process virtualServices
+		for _, virtualService := range virtualServices {
+			log.Printf("Discovered virtual service named %s in namespace %s", virtualService.Name, namespace.Name)
+		}
+	}
+
+	// no data is feeded to future steps or stages
 	return &pipeline.Result{
-		Error:  nil,
-		Data:   struct{ msg string }{},
-		KeyVal: map[string]interface{}{},
+		Error: nil,
 	}
 }
 
