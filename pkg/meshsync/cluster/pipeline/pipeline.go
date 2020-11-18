b5cabc79fc7dce7e8d8c6c0d74e83a390b41fb90
Dev Kalra <kalradev@outlook.com>
2020-11-18 15:50:17 +0530

    added steps to pipeline
    Signed-off-by: Dev Kalra <kalradev@outlook.com>

--- pkg/meshsync/cluster/pipeline/pipeline.go
+++ pkg/meshsync/cluster/pipeline/pipeline.go
@@ -15,13 +15,26 @@ var (
 		Concurrent: true,
 		Steps:      []pipeline.Step{},
 	}
+
+	LocalDiscoveryStage = &pipeline.Stage{
+		Name:       "Local-Resource-Discovery",
+		Concurrent: true,
+		Steps:      []pipeline.Step{},
+	}
 )
 
 func (cluster *Cluster) InitializePipeline() (*pipeline.Pipeline, error) {
 
-	// Mesh Discovery Stage
+	// Global discovery
 	gdstage := GlobalDiscoveryStage
 	gdstage.AddStep(cluster)
+	gdstage.AddStep(NewNode(cluster.client))
+	gdstage.AddStep(NewNamespace(cluster.client))
+
+	// Local discovery
+	ldstage := LocalDiscoveryStage
+	ldstage.AddStep(NewDeployment(cluster.client))
+	ldstage.AddStep(NewPod(cluster.client))
 
 	// Create Pipeline
 	clusterPipeline := ClusterPipeline
