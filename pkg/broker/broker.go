517907502c371e7d3a7aff2ab82ec157e80517ce
Dev Kalra <kalradev@outlook.com>
2020-11-24 19:10:50 +0530

    broker: nats implementation
    Signed-off-by: Dev Kalra <kalradev@outlook.com>

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -4,14 +4,19 @@ import (
 	"github.com/layer5io/meshery-operator/pkg/broker/nats"
 )
 
+type Message struct {
+	Type   string
+	Object interface{}
+}
+
 type PublishInterface interface {
-	Publish(string, interface{})
-	PublishWithCallback(string, string, interface{})
+	Publish(string, interface{}) error
+	PublishWithCallback(string, string, interface{}) error
 }
 
 type SubscribeInterface interface {
-	Subscribe(string, string)
-	SubscribeWithHandler(string, string)
+	Subscribe(string, string) error
+	SubscribeWithHandler(string, string) error
 }
 
 type Broker interface {
