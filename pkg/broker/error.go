b64ef97e1fbff3139e272956b5e065b411e87f4b
dhruv0000 <patel.4@iitj.ac.in>
2021-06-25 18:53:54 +0530

    Added Long description
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- pkg/broker/error.go
+++ pkg/broker/error.go
@@ -12,17 +12,17 @@ const (
 )
 
 func ErrGettingResource(err error) error {
-	return errors.New(ErrGettingResourceCode, errors.Alert, []string{"Unable to get requested resource" + err.Error()}, []string{"Unable to get requested resource while doing health check", err.Error()}, []string{}, []string{})
+	return errors.New(ErrGettingResourceCode, errors.Alert, []string{"Unable to get requested resource"}, []string{"Unable to get requested resource while doing health check", err.Error()}, []string{}, []string{})
 }
 
 func ErrGettingEndpoint(err error) error {
-	return errors.New(ErrGettingEndpointCode, errors.Alert, []string{"Unable to discovery endpoint" + err.Error()}, []string{}, []string{}, []string{})
+	return errors.New(ErrGettingEndpointCode, errors.Alert, []string{"Unable to discovery endpoint"}, []string{err.Error()}, []string{}, []string{})
 }
 
 func ErrReplicasNotReady(reason string) error {
-	return errors.New(ErrReplicasNotReadyCode, errors.Alert, []string{"Replicas not ready.", reason}, []string{}, []string{}, []string{})
+	return errors.New(ErrReplicasNotReadyCode, errors.Alert, []string{"Replicas not ready."}, []string{reason}, []string{}, []string{})
 }
 
 func ErrConditionFalse(reason string) error {
-	return errors.New(ErrConditionFalseCode, errors.Alert, []string{"Health check condition false.", reason}, []string{}, []string{}, []string{})
+	return errors.New(ErrConditionFalseCode, errors.Alert, []string{"Health check condition false."}, []string{reason}, []string{}, []string{})
 }
