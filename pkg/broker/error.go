cccf790973ea68dcedefeecca4be11f049379be4
dhruv0000 <patel.4@iitj.ac.in>
2021-06-25 18:09:58 +0530

    Updated Meshkit errors
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- pkg/broker/error.go
+++ pkg/broker/error.go
@@ -12,17 +12,17 @@ const (
 )
 
 func ErrGettingResource(err error) error {
-	return errors.NewDefault(ErrGettingResourceCode, "Unable to get requested resource", err.Error())
+	return errors.New(ErrGettingResourceCode, errors.Alert, []string{"Unable to get requested resource" + err.Error()}, []string{"Unable to get requested resource while doing health check", err.Error()}, []string{}, []string{})
 }
 
 func ErrGettingEndpoint(err error) error {
-	return errors.NewDefault(ErrGettingEndpointCode, "Unable to discovery endpoint", err.Error())
+	return errors.New(ErrGettingEndpointCode, errors.Alert, []string{"Unable to discovery endpoint" + err.Error()}, []string{}, []string{}, []string{})
 }
 
 func ErrReplicasNotReady(reason string) error {
-	return errors.NewDefault(ErrReplicasNotReadyCode, "Replicas not ready", reason)
+	return errors.New(ErrReplicasNotReadyCode, errors.Alert, []string{"Replicas not ready.", reason}, []string{}, []string{}, []string{})
 }
 
 func ErrConditionFalse(reason string) error {
-	return errors.NewDefault(ErrConditionFalseCode, reason)
+	return errors.New(ErrConditionFalseCode, errors.Alert, []string{"Health check condition false.", reason}, []string{}, []string{}, []string{})
 }
