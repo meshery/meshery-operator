40a0bc27f73fb429cc26ee56b7cd06173867cb65
Rudraksh Pareek <rudrakshpareek3601@gmail.com>
2021-08-21 20:25:44 +0530

    fix failing operator tests
    Signed-off-by: Rudraksh Pareek <rudrakshpareek3601@gmail.com>

--- scripts/setup-ci.sh
+++ scripts/setup-ci.sh
@@ -6,10 +6,7 @@
 
 os=$(go env GOOS)
 arch=$(go env GOARCH)
-TESTBIN_DIR=testbin
 
-# download kubebuilder and extract it to tmp
-curl -L https://go.kubebuilder.io/dl/2.3.1/${os}/${arch} | tar -xz -C /tmp/
-mkdir $TESTBIN_DIR
-sudo mv /tmp/kubebuilder_2.3.1_${os}_${arch} /usr/local/kubebuilder
-export PATH=$TESTBIN_DIR/kubebuilder/bin
+# download kubebuilder and install locally.
+curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env G      OARCH)
+chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
