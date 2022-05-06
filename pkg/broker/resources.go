8d0fa40dabb153b9d9d7371fcf0e37cabea8276f
ashish <ashishjaitiwari15112000@gmail.com>
2022-05-06 22:05:38 +0530

    Update nats image
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- pkg/broker/resources.go
+++ pkg/broker/resources.go
@@ -166,7 +166,7 @@ ACSU3Q6LTLBVLGAQUONAGXJHVNWGSKKAUA7IY5TB4Z7PLEKSR5O6JTGR: eyJ0eXAiOiJqd3QiLCJhbG
 			Containers: []corev1.Container{
 				{
 					Name:            "nats",
-					Image:           "nats:latest-alpine3.12",
+					Image:           "nats:2.8.2-alpine3.15",
 					ImagePullPolicy: corev1.PullIfNotPresent,
 					Ports: []corev1.ContainerPort{
 						{
