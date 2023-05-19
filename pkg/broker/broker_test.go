6b04fc208c45550ed0d3d37560798250ee37fbbf
Aisuko <urakiny@gmail.com>
2023-05-19 07:12:17 +0000

    feat: add suit test casses for broker
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ pkg/broker/broker_test.go
@@ -0,0 +1,161 @@
+/*
+Copyright 2023 Layer5, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package broker
+
+import (
+	"context"
+
+	mesheryv1alpha1 "github.com/layer5io/meshery-operator/api/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("Broker funtions test cases", func() {
+
+	var (
+		ctx context.Context
+	)
+
+	BeforeEach(func() {
+		ctx = context.TODO()
+	})
+
+	Context("Test for GetObjects function", func() {
+		It("should return the map of objects", func() {
+			m := &mesheryv1alpha1.Broker{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+				},
+				Spec: mesheryv1alpha1.BrokerSpec{
+					Size: 1,
+				},
+			}
+			obj := GetObjects(m)
+			Expect(obj).ToNot(BeNil())
+
+			By("checking server config")
+			Expect(obj[ServerConfig]).ToNot(BeNil())
+
+			By("checking account config")
+			Expect(obj[AccountConfig]).ToNot(BeNil())
+
+			By("checking server object, namespace and name, replicas")
+			Expect(obj[ServerObject]).ToNot(BeNil())
+			Expect(obj[ServerObject].GetNamespace()).To(Equal(m.ObjectMeta.Namespace))
+			Expect(obj[ServerObject].GetName()).To(Equal(m.ObjectMeta.Name))
+		})
+	})
+
+	Context("Test for CheckHealth function", func() {
+		It("should return nil", func() {
+
+			namespace := "default"
+			name := "default"
+			m := &mesheryv1alpha1.Broker{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Spec: mesheryv1alpha1.BrokerSpec{
+					Size: 1,
+				},
+			}
+
+			// create a statefulSet object in the cluster
+			s := &v1.StatefulSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: namespace,
+					Name:      name,
+				},
+				Spec: v1.StatefulSetSpec{
+					Replicas: &m.Spec.Size,
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"app": "broker",
+						},
+					},
+					Template: corev1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: name,
+							Labels: map[string]string{
+								"app": "broker",
+							},
+						},
+					},
+				},
+			}
+			By("Creating statefulset resources for testing broker")
+			err := k8sClient.Create(ctx, s)
+			Expect(err).ToNot(HaveOccurred())
+			By("Checking if the broker is healthy, it should be successful")
+			Expect(CheckHealth(ctx, m, clientSet)).To(Succeed())
+
+		})
+	})
+
+	Context("Test for GetEndpoint function", func() {
+		It("should return the endpoint", func() {
+
+			name := "default"
+			namespace := "default"
+			m := &mesheryv1alpha1.Broker{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Spec: mesheryv1alpha1.BrokerSpec{
+					Size: 1,
+				},
+			}
+
+			By("Create service first")
+			s := &corev1.Service{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Spec: corev1.ServiceSpec{
+					Type: corev1.ServiceTypeNodePort,
+					Ports: []corev1.ServicePort{
+						{
+							Name: "http",
+							Port: 8080,
+						},
+						{
+							NodePort: 30002,
+							Name:     "grpc",
+							Port:     8082,
+						},
+					},
+				},
+			}
+			err := k8sClient.Create(ctx, s)
+			Expect(err).ToNot(HaveOccurred())
+
+			url := "http://localhost:8080"
+			Expect(GetEndpoint(ctx, m, clientSet, url)).ShouldNot(HaveOccurred())
+
+			By("checking m.status.endpoint")
+			Expect(m.Status.Endpoint.External).To(Equal("localhost:30002"))
+			Expect(m.Status.Endpoint.Internal).Should(ContainSubstring("8082"))
+		})
+	})
+})
---
+++ pkg/broker/suit_test.go
@@ -0,0 +1,111 @@
+/*
+Copyright 2023 Layer5, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package broker
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	apiv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	types "k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	k8sscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	mesheryv1alpha1 "github.com/layer5io/meshery-operator/api/v1alpha1"
+)
+
+// Initialize test suite entrypoint
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Broker Suite")
+}
+
+var (
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	clientSet *kubernetes.Clientset
+)
+
+var _ = BeforeSuite(func(ctx SpecContext) {
+	logf.SetLogger(zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter)))
+
+	By("bootstrapping test environment")
+	timeout := 3 * time.Minute
+	testEnv = &envtest.Environment{
+		ErrorIfCRDPathMissing: true,
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "config", "crd", "bases"),
+		},
+		ControlPlaneStartTimeout: timeout,
+		ControlPlaneStopTimeout:  timeout,
+		AttachControlPlaneOutput: false,
+		BinaryAssetsDirectory:    filepath.Join("..", "..", "bin", "k8s", "1.24.2-linux-amd64"),
+	}
+
+	var cfg *rest.Config
+	var err error
+	done := make(chan interface{})
+	go func() {
+		defer GinkgoRecover()
+		cfg, err = testEnv.Start()
+		close(done)
+	}()
+	Eventually(done).WithContext(ctx).WithTimeout(timeout).Should(BeClosed())
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	scheme := runtime.NewScheme()
+
+	Expect(mesheryv1alpha1.AddToScheme(scheme)).To(Succeed())
+	Expect(k8sscheme.AddToScheme(scheme)).To(Succeed())
+	Expect(apiv1.AddToScheme(scheme)).To(Succeed())
+
+	// +kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+
+	clientSet, err = kubernetes.NewForConfig(cfg)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(clientSet).NotTo(BeNil())
+
+	crd := &apiv1.CustomResourceDefinition{}
+
+	err = k8sClient.Get(ctx, types.NamespacedName{Name: "meshsyncs.meshery.layer5.io"}, crd)
+	Expect(err).NotTo(HaveOccurred())
+	err = k8sClient.Get(ctx, types.NamespacedName{Name: "brokers.meshery.layer5.io"}, crd)
+	Expect(err).NotTo(HaveOccurred())
+	Expect(crd.Spec.Names.Kind).To(Equal("Broker"))
+})
+
+var _ = AfterSuite(func() {
+	err := testEnv.Stop()
+	if err != nil {
+		os.Exit(1)
+	}
+})
