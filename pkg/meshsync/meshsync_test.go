e84403581e28e7d87eec02b7517370ef29b1dffa
Aisuko <urakiny@gmail.com>
2023-05-22 06:01:00 +0000

    Add test for meshsync
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- pkg/meshsync/meshsync_test.go
+++ pkg/meshsync/meshsync_test.go
@@ -17,33 +17,27 @@ limitations under the License.
 package meshsync
 
 import (
+	"testing"
+
 	mesheryv1alpha1 "github.com/layer5io/meshery-operator/api/v1alpha1"
-	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
-const (
-	ServerObject = "server-object"
-)
-
-type Object interface {
-	runtime.Object
-	metav1.Object
-}
-
-func GetObjects(m *mesheryv1alpha1.MeshSync) map[string]Object {
-	return map[string]Object{
-		ServerObject: getServerObject(m.ObjectMeta.Namespace, m.ObjectMeta.Name, m.Spec.Size, m.Status.PublishingTo),
+func TestGetObjects(t *testing.T) {
+	m := &mesheryv1alpha1.MeshSync{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "test",
+		},
+		Spec: mesheryv1alpha1.MeshSyncSpec{
+			Size: 1,
+		},
 	}
-}
-
-func getServerObject(namespace, name string, replicas int32, url string) Object {
-	var obj = &v1.Deployment{}
-	Deployment.DeepCopyInto(obj)
-	obj.ObjectMeta.Namespace = namespace
-	obj.ObjectMeta.Name = name
-	obj.Spec.Replicas = &replicas
-	obj.Spec.Template.Spec.Containers[0].Env[0].Value = url // Set broker endpoint
-	return obj
+	obj := GetObjects(m)
+	if obj == nil {
+		t.Error("GetObjects returned nil")
+	}
+	if obj[ServerObject] == nil {
+		t.Error("GetObjects returned nil for server object")
+	}
 }
