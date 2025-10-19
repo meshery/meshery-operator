d7dcb378e0b2ced68f25004a6a0d333ed5a836f3
dharam <dharmendra.20208039@mnnit.ac.in>
2025-10-19 11:25:24 +0530

    chore(broker): bump NATS image to 2.10.17-alpine (closest valid tag to alpine3.19)
    - alpine3.19 tag for 2.10.17 does not exist on Docker Hub
    - use nats:2.10.17-alpine which is published
    - avoids ImagePullBackOff in CI
    
    Signed-off-by: dharam <dharmendra.20208039@mnnit.ac.in>

--- pkg/broker/resources.go
+++ pkg/broker/resources.go
@@ -175,7 +175,7 @@ ACSU3Q6LTLBVLGAQUONAGXJHVNWGSKKAUA7IY5TB4Z7PLEKSR5O6JTGR: eyJ0eXAiOiJqd3QiLCJhbG
 			Containers: []corev1.Container{
 				{
 					Name:            "nats",
-					Image:           "nats:2.10.14-alpine",
+					Image:           "nats:2.10.17-alpine",
 					ImagePullPolicy: corev1.PullIfNotPresent,
 					Ports: []corev1.ContainerPort{
 						{
